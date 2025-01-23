f6476d3b28a6064b08ae207888aaaff4def70517
saranya-dara-13 <2311cs010160@mallareddyuniversity.ac.in>
2025-01-23 19:05:28 +0530

    Add files via upload

---
+++ MultiplyDigits.java
@@ -0,0 +1,22 @@
+package mru;
+
+public class MultiplyDigits {
+
+	public static void main(String[] args) {
+	
+				int[] originalArray = {12, 34, 56};
+		        int[] resultArray = new int[originalArray.length];
+		        for (int i = 0; i < originalArray.length; i++) {
+		            int product = 1;
+		            int number = originalArray[i];
+		            while (number > 0) {
+		                product *= number % 10; 
+		                number /= 10;             
+		            }
+		            resultArray[i] = product;
+		        }
+		        for (int num : resultArray) {
+		            System.out.print(num + " ");
+		        }
+		    }
+		}
---
+++ ReverseArray.java
@@ -0,0 +1,24 @@
+package mru;
+
+public class ReverseArray {
+
+	public static void main(String[] args) {
+
+				int[] originalArray = {101, 102, 103, 104, 105};
+		        int left = 0;
+		        int right = originalArray.length - 1;
+		        
+		        while (left < right) {
+		            int temp = originalArray[left];
+		            originalArray[left] = originalArray[right];
+		            originalArray[right] = temp;
+		            left++;
+		            right--;
+		        }
+		        for (int num : originalArray) {
+		            System.out.print(num + " ");
+		        }
+		    }
+
+}
+
