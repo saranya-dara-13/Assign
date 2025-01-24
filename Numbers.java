7e48f02ab1f0e3a208dfc223fcfc2e16dc8a8591
saranya-dara-13 <2311cs010160@mallareddyuniversity.ac.in>
2025-01-24 12:50:37 +0530

    Add files via upload

---
+++ Numbers.java
@@ -0,0 +1,21 @@
+package day;
+
+		public class Numbers {
+
+			public static void main(String[] args) {
+				        int[] inputArray = {1, 2, 3, 4, 5, 6}; // The original array
+				        int[] outputArray = new int[3]; // Output array of size 3
+
+				        // Fill the output array with the value 7
+				        for (int i = 0; i < outputArray.length; i++) {
+				            outputArray[i] = 7;
+				        }
+
+				        // Print the output array
+				        for (int value : outputArray) {
+				            System.out.print(value + " ");
+				        }
+
+	}
+
+}
