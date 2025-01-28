6abe23abf3ed21690756c1ebf5cdc8cc71d547e5
saranya-dara-13 <2311cs010160@mallareddyuniversity.ac.in>
2025-01-28 18:38:45 +0530

    Add files via upload

--- ReverseArray.java
+++ ReverseArray.java
@@ -1,24 +1,24 @@
-package mru;
+package com.mru.faqs;
 
 public class ReverseArray {
 
 	public static void main(String[] args) {
 
-				int[] originalArray = {101, 102, 103, 104, 105};
-		        int left = 0;
-		        int right = originalArray.length - 1;
-		        
-		        while (left < right) {
-		            int temp = originalArray[left];
-		            originalArray[left] = originalArray[right];
-		            originalArray[right] = temp;
-		            left++;
-		            right--;
-		        }
-		        for (int num : originalArray) {
-		            System.out.print(num + " ");
-		        }
-		    }
+						int[] originalArray = {101, 102, 103, 104, 105};
+				        int left = 0;
+				        int right = originalArray.length - 1;
+				        
+				        while (left < right) {
+				            int temp = originalArray[left];
+				            originalArray[left] = originalArray[right];
+				            originalArray[right] = temp;
+				            left++;
+				            right--;
+				        }
+				        for (int num : originalArray) {
+				            System.out.print(num + " ");
+				        }
+				    }
 
-}
+	}
 
